67e251059de68ba00902c5bedabce98e2e0de5f4
Louis Qian <yuq4@illinois.edu>
2024-09-17 02:57:36 -0500

    feat: initial args layout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// title of homework
+    #[arg(short, long)]
+    title: String,
+
+    /// Course Number
+    #[arg(short, long)]
+    course: String,
+
+    /// No. of questions
+    #[arg(short, long)]
+    length: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{args:#?}");
 }
